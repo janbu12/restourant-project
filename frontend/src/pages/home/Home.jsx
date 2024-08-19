f491ad3408656666efbcf0ecbf50a9fb5dbd313d
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-08-19 17:56:31 +0700

    test1

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
-      <div>Login</div>
+      <Link to={"/login"}>Login</Link>
     </>
   )
 }
