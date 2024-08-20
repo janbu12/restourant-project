f139f8bf1f17994807c99450de53e1cee856d811
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-08-20 14:27:21 +0700

    responsive navbar, menu, partner

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Toggle menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className='laptop:flex w-full h-auto laptop:justify-between laptop:items-center bg-white drop-shadow-md py-2 px-24'>
-       <div className=''>
-        <h2 className='text-lg font-bold text-orange-950'>Logo.</h2>
-       </div>
-       <div className='laptop:flex gap-2 '>
-        <a href="#home" className='laptop:nav-link'>Home</a>
-        <a href="#menu" className='laptop:nav-link'>Menu</a>
-        <a href="#" className='laptop:nav-link'>About Us</a>
-        <a href="#partner" className='laptop:nav-link'>Partner</a>
-        <a href="#" className='laptop:nav-link'>Career</a>
-       </div>
-       <div className=''>
-       </div>
-    </div>
+    <>
+      <div className='phone:hidden laptop:flex w-full h-auto laptop:justify-between laptop:items-center bg-white drop-shadow-md py-2 px-24'>
+        <div className=''>
+          <h2 className='text-lg font-bold text-orange-950'>Logo.</h2>
+        </div>
+        <div className='laptop:flex gap-2 '>
+          <a href="#home" className='laptop:nav-link'>Home</a>
+          <a href="#menu" className='laptop:nav-link'>Menu</a>
+          <a href="#" className='laptop:nav-link'>About Us</a>
+          <a href="#partner" className='laptop:nav-link'>Partner</a>
+          <a href="#" className='laptop:nav-link'>Career</a>
+        </div>
+        <div className=''>
+        </div>
+      </div>
+      <div className='phone:flex laptop:hidden w-full h-auto justify-between items-center fixed z-50 top-0 bg-white drop-shadow-md py-3 px-8'>
+        <div className=''>
+            <h2 className='text-2xl font-bold text-orange-950'>Logo.</h2>
+        </div>
+        <div>
+          <button 
+            onClick={toggleMenu}
+            className='flex outline-none hover:text-orange-700 transition-all duration-200'
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='size-10'>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div 
+        className={`phone:flex laptop:hidden w-full bg-white drop-shadow-md py-2 px-8 fixed top-0 right-0 z-30 ${isMenuOpen ? 'animate-slide-in' : 'animate-slide-out'}`}>
+        <div className='flex flex-col w-full justify-center items-center mt-20 gap-3 text-orange-950'>
+          <a href="#home" className='text-center marker:py-2 text-lg font-bold' onClick={toggleMenu}>Home</a>
+          <a href="#menu" className='text-center py-2 text-lg font-bold' onClick={toggleMenu}>Menu</a>
+          <a href="#" className='text-center py-2 text-lg font-bold' onClick={toggleMenu}>About Us</a>
+          <a href="#partner" className='py-2 text-lg font-bold' onClick={toggleMenu}>Partner</a>
+          <a href="#" className='text-center py-2 text-lg font-bold' onClick={toggleMenu}>Career</a>
+        </div>
+      </div>
+    </>
   )
 }
--- frontend/src/pages/landing_page/LandingPage.jsx
+++ frontend/src/pages/landing_page/LandingPage.jsx
@@ -64,7 +64,7 @@ export default function LandingPage() {
   return (
     <>
       <Navbar/>
-      <div className='bg-slate-50 w-full h-auto my-12 px-24' id='menu'>
+      <div className='bg-slate-50 w-full h-auto laptop:my-12 phone:px-8 laptop:px-24  phone:mt-24' id='menu'>
         {isLoading ? (
           <div className='flex justify-center items-center h-screen'>
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border
@@ -74,11 +74,11 @@ export default function LandingPage() {
         ) : (
           <>
           <div className='mb-6'>
-            <h2 className='text-7xl font-semibold text-orange-700'>Our <span className='text-orange-950'>Menu!</span></h2>
+            <h2 className='laptop:text-7xl phone:text-5xl font-semibold text-orange-700'>Our <span className='text-orange-950'>Menu!</span></h2>
           </div>
           <div className='flex flex-wrap justify-around items-center gap-5'>
             {menu.map((item, index) => (
-              <div key={index} className='w-1/2 md:w-1/3
+              <div key={index} className='w-1/2 phone:w-full md:w-1/3
               laptop:w-1/5 xl:w-1/5'>
                 <div className='flex flex-col bg-white rounded-lg shadow-md p-4 gap-3 laptop:min-h-80 laptop:max-h-80 justify-center'>
                   <h2 className='text-lg font-semibold text-orange-950'>{item.name}</h2>
@@ -97,13 +97,16 @@ export default function LandingPage() {
       {/* <div className='bg-red-500 w-full h-screen' id='home'>
 
       </div> */}
-      <div className='w-full px-24 py-8' id='partner'>
-        <div className='flex justify-center items-center gap-24'>
-          <div className='flex flex-col w-1/2 gap-12'>
+      <div className='w-full phone:px-8 laptop:px-24 py-8' id='partner'>
+        <div className='flex laptop:flex-row phone:flex-col laptop:justify-between laptop:items-center laptop:gap-6'>
+          <div className='flex flex-col laptop:w-max-80% gap-12'>
+            <div className='flex flex-col w-full laptop:hidden phone:block '>
+              <img src="./partner.jpeg" alt="partner.jpg" className='rounded-xl drop-shadow w-full max-h-72 object-cover' />
+            </div>
             <div className='flex flex-col'>
-              <h2 className='text-7xl font-medium text-orange-950'>Partnership</h2>
-              <h2 className='mb-3 text-5xl font-medium text-orange-700'>Opportunity!</h2>
-              <p className='text-lg'>Sampaikan bagaimana kami bisa bekerja sama untuk mencapai tujuan Anda.</p>
+              <h2 className='laptop:text-7xl font-medium text-orange-950 phone:text-5xl'>Partnership</h2>
+              <h2 className='mb-3 laptop:text-5xl font-medium text-orange-700 phone:text-3xl'>Opportunity!</h2>
+              <p className='laptop:text-lg phone:text-base'>Sampaikan bagaimana kami bisa bekerja sama untuk mencapai tujuan Anda.</p>
             </div>
             <form className='flex flex-col gap-3'>
               <div className='flex flex-col gap-2'>
@@ -161,7 +164,7 @@ export default function LandingPage() {
               </div>
             </form>
           </div>
-          <div className='flex flex-col w-1/2'>
+          <div className='flex laptop:block phone:hidden'>
             <img src="./partner.jpeg" alt="partner.jpg" className='rounded-xl drop-shadow-lg max-h-[900px]' />
           </div>
         </div>
