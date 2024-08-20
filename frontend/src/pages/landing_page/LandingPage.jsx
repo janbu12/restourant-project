8e2c841d25a36a252cd6b32abcdee57931219839
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-08-20 15:03:11 +0700

    fix loading bug

--- frontend/src/pages/landing_page/LandingPage.jsx
+++ frontend/src/pages/landing_page/LandingPage.jsx
@@ -125,8 +125,7 @@ export default function LandingPage() {
   return (
     <>
       <Navbar/>
-      <div className='bg-slate-50 w-full h-auto laptop:my-12 phone:px-8 laptop:px-24  phone:mt-24' id='menu'>
-        {isLoading ? (
+      {isLoading ? (
           <div className='flex justify-center items-center h-screen'>
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border
             -b-2 border-gray-900">
@@ -134,103 +133,106 @@ export default function LandingPage() {
           </div>
         ) : (
           <>
-          <div className='mb-6'>
-            <h2 className='laptop:text-7xl phone:text-5xl font-semibold text-orange-700'>Our <span className='text-orange-950'>Menu!</span></h2>
-          </div>
-          <div className='flex flex-wrap justify-between items-center gap-5'>
-            {menu.map((item, index) => (
-              <div key={index} className='w-1/2 phone:w-full md:w-1/3
-              laptop:w-1/5 xl:w-1/5'>
-                <div className='flex flex-col bg-white rounded-lg shadow-md p-4 gap-3 laptop:min-h-80 laptop:max-h-80 justify-center'>
-                  <h2 className='text-lg font-semibold text-orange-950'>{item.name}</h2>
-                  <p className='text-sm text-justify'>{item.description}</p>
-                  <p className='text-sm text-orange-700'>Rp. {item.price}</p>
-                  <Link to={`/menu/${item.id}`} className='text-sm text-blue-500 hover:text
-                  -blue-700'></Link>
+            <div className='bg-slate-50 w-full h-auto laptop:my-12 phone:px-8 laptop:px-24  phone:mt-24' id='menu'>
+            <>
+            <div className='mb-6'>
+              <h2 className='laptop:text-7xl phone:text-5xl font-semibold text-orange-700'>Our <span className='text-orange-950'>Menu!</span></h2>
+            </div>
+            <div className='flex flex-wrap justify-between items-center gap-5'>
+              {menu.map((item, index) => (
+                <div key={index} className='w-1/2 phone:w-full md:w-1/3
+                laptop:w-1/5 xl:w-1/5'>
+                  <div className='flex flex-col bg-white rounded-lg shadow-md p-4 gap-3 laptop:min-h-80 laptop:max-h-80 justify-center'>
+                    <h2 className='text-lg font-semibold text-orange-950'>{item.name}</h2>
+                    <p className='text-sm text-justify'>{item.description}</p>
+                    <p className='text-sm text-orange-700'>Rp. {item.price}</p>
+                    <Link to={`/menu/${item.id}`} className='text-sm text-blue-500 hover:text
+                    -blue-700'></Link>
+                  </div>
                 </div>
-              </div>
-              ))}
-          </div>
-          </>
-        )
-        }
-      </div>
-      {/* <div className='bg-red-500 w-full h-screen' id='home'>
+                ))}
+            </div>
+            </>
+        </div>
+        {/* <div className='bg-red-500 w-full h-screen' id='home'>
 
-      </div> */}
-      <div className='w-full phone:px-8 laptop:px-24 py-8' id='partner'>
-        <div className='flex laptop:flex-row phone:flex-col laptop:justify-between laptop:items-center laptop:gap-6'>
-          <div className='flex flex-col laptop:w-max-80% gap-12'>
-            <div className='flex flex-col w-full laptop:hidden phone:block '>
-              <img src="./partner.jpeg" alt="partner.jpg" className='rounded-xl drop-shadow w-full max-h-72 object-cover' />
-            </div>
-            <div className='flex flex-col'>
-              <h2 className='laptop:text-7xl font-medium text-orange-950 phone:text-5xl'>Partnership</h2>
-              <h2 className='mb-3 laptop:text-5xl font-medium text-orange-700 phone:text-3xl'>Opportunity!</h2>
-              <p className='laptop:text-lg phone:text-base'>Sampaikan bagaimana kami bisa bekerja sama untuk mencapai tujuan Anda.</p>
-            </div>
-            <form className='flex flex-col gap-3' onSubmit={handleSubmitPartner}>
-              <div className='flex flex-col gap-2'>
-                <label htmlFor="">Name</label>
-                <input
-                  required 
-                  type="text" 
-                  className="w-full py-2 px-4 border border-gray-400 rounded" 
-                  value={name}
-                  onChange={handleChangeName}
-                />
+        </div> */}
+        <div className='w-full phone:px-8 laptop:px-24 py-8' id='partner'>
+          <div className='flex laptop:flex-row phone:flex-col laptop:justify-between laptop:items-center laptop:gap-6'>
+            <div className='flex flex-col laptop:w-max-80% gap-12'>
+              <div className='flex flex-col w-full laptop:hidden phone:block '>
+                <img src="./partner.jpeg" alt="partner.jpg" className='rounded-xl drop-shadow w-full max-h-72 object-cover' />
               </div>
-              <div className='flex flex-col gap-2'>
-                <label htmlFor="">Email</label>
-                <input
-                  required
-                  type="email" 
-                  className="w-full py-2 px-4 border border-gray-400 rounded"
-                  value={email}
-                  onChange={handleChangeEmail}
-                  title="Please enter a valid email address" 
-                />
+              <div className='flex flex-col'>
+                <h2 className='laptop:text-7xl font-medium text-orange-950 phone:text-5xl'>Partnership</h2>
+                <h2 className='mb-3 laptop:text-5xl font-medium text-orange-700 phone:text-3xl'>Opportunity!</h2>
+                <p className='laptop:text-lg phone:text-base'>Sampaikan bagaimana kami bisa bekerja sama untuk mencapai tujuan Anda.</p>
               </div>
-              <div className='flex flex-col gap-2'>
-                <label htmlFor="">Phone Number</label>
-                <input
-                  required
-                  type="text" 
-                  className="w-full py-2 px-4 border border-gray-400 rounded"
-                  value={phone}
-                  onChange={handleChangePhone}
-                />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <label htmlFor="">Message</label>
-                <textarea
-                  required
-                  value={text} 
-                  className='resize-none w-full py-2 px-4 border border-gray-400 rounded' 
-                  name="" 
-                  id="" 
-                  rows="8"
-                  onChange={handleChangeMessage}
-                ></textarea>
-                <div>
-                  <p>{text.length} / {maxLength} Character</p>
+              <form className='flex flex-col gap-3' onSubmit={handleSubmitPartner}>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="">Name</label>
+                  <input
+                    required 
+                    type="text" 
+                    className="w-full py-2 px-4 border border-gray-400 rounded" 
+                    value={name}
+                    onChange={handleChangeName}
+                  />
                 </div>
-              </div>
-              <div>
-                <button
-                  className='bg-orange-700 py-3 w-full rounded-xl text-white hover:bg-orange-800 transition-all duration-300'
-                  type='submit'
-                >
-                  Send
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className='flex laptop:block phone:hidden'>
-            <img src="./partner.jpeg" alt="partner.jpg" className='rounded-xl drop-shadow-lg max-h-[900px]' />
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="">Email</label>
+                  <input
+                    required
+                    type="email" 
+                    className="w-full py-2 px-4 border border-gray-400 rounded"
+                    value={email}
+                    onChange={handleChangeEmail}
+                    title="Please enter a valid email address" 
+                  />
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="">Phone Number</label>
+                  <input
+                    required
+                    type="text" 
+                    className="w-full py-2 px-4 border border-gray-400 rounded"
+                    value={phone}
+                    onChange={handleChangePhone}
+                  />
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="">Message</label>
+                  <textarea
+                    required
+                    value={text} 
+                    className='resize-none w-full py-2 px-4 border border-gray-400 rounded' 
+                    name="" 
+                    id="" 
+                    rows="8"
+                    onChange={handleChangeMessage}
+                  ></textarea>
+                  <div>
+                    <p>{text.length} / {maxLength} Character</p>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    className='bg-orange-700 py-3 w-full rounded-xl text-white hover:bg-orange-800 transition-all duration-300'
+                    type='submit'
+                  >
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className='flex laptop:block phone:hidden'>
+              <img src="./partner.jpeg" alt="partner.jpg" className='rounded-xl drop-shadow-lg max-h-[900px]' />
+            </div>
           </div>
         </div>
-      </div>
+          </>
+        )
+      }
     </>
   )
 }
