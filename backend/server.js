ec5280faf4cfa586b140183b417e46d6c721c00c
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2024-09-02 01:52:23 +0700

    edit server

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
